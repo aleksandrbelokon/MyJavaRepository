9c0374cd15c74cac08c90c004cf78946809a4da1
aleksandrbelokon <aleksandrbelokon@yandex.ru>
2021-10-07 20:58:00 +0300

    Малышка очень вредная кошка
    Eclipse Git Test

--- javaHelloWorld/src/javaHelloWorld/HelloWorld.java
+++ javaHelloWorld/src/javaHelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Тестовая программа Hello World!!!");
+			System.out.println("Малышка вредная кошка.");
 	}
 
 }
